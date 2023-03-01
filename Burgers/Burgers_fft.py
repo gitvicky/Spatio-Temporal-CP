4a29b173f7b416d61525e4922d461d1967d01a5b
gitvicky <vignesh7g@gmail.com>
2023-03-01 15:52:56 +0000

    Cleanup and add burgers

---
+++ Burgers/Burgers_fft.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 1st March 2023
+
+@author: vgopakum 
+
+---------------------------------------------------------------------------------------
+1D Burgers' Equation using the spectral method in Python 
+---------------------------------------------------------------------------------------
+
+Code inspired from from Steve Brunton's FFT Example videos :
+https://www.youtube.com/watch?v=hDeARtZdq-U&t=639s
+
+Equation:
+u_t + u*u_x =  nu*u_xx on [-1,1] x [-1,1]
+
+Boundary Conditions: 
+Periodic (implicit via FFT)
+
+Initial Distribution :
+u(x,y,t=0) = sin(alpha*pi*x) + np.cos(beta*np.pi*-x) + 1/np.cosh(gamma*np.pi*x) 
+where alpha, beta, gamma all lie within the domain [-3,3]^3
+
+Initial Velocity Condition :
+u_t(x,y,t=0) = 0
+
+"""
+# %%
+#Importing the necessary packages. 
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy.integrate import odeint
+from simvue import run 
+
+def solve_burgers(configuration):
+
+    nu = configuration['viscosity']
+    L = configuration['domain length']
+    N = configuration['discretisation']
+    dt = configuration['dt']
+    iterations = configuration['time iteration']
+
+
+    dx = L/N 
+    x = np.arange(0, L, dx) #Define the X Domain 
+    midpoint = int(N/2)
+
+    #Define the discrete wavenumbers 
+    kappa = 2*np.pi*np.fft.fftfreq(N, d=dx)
+
+    #Initial Condition
+    alpha = configuration['alpha']
+    beta = configuration['beta']
+    gamma = configuration['gamma']
+    u0 = np.sin(alpha*np.pi*x) + np.cos(beta*np.pi*-x) + 1/np.cosh(gamma*np.pi*x) 
+
+    #Simulate in Fourier Freq domain. 
+    t= np.arange(0, iterations*dt, dt)
+
+    def rhsBurgers(u, t, kappa, nu):
+        uhat = np.fft.fft(u)
+        d_uhat = (1j)*kappa*uhat
+        dd_uhat = -np.power(kappa, 2)*uhat
+        d_u = np.fft.ifft(d_uhat)
+        dd_u = np.fft.ifft(dd_uhat)
+        du_dt = -u*d_u + nu*dd_u
+
+        run.log_metrics({'u_mid': uhat.real})
+                        
+        return du_dt.real
+    
+    u = odeint(rhsBurgers, u0, t, args=(kappa, nu))
+    
+    return u
+
+
+# %%
---
+++ Burgers/Data_Gen.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 1st March 2023
+
+@author: vgopakum 
+
+---------------------------------------------------------------------------------------
+Data Generation for 1D Burgers Surrogate
+---------------------------------------------------------------------------------------
+
+Code inspired from from Steve Brunton's FFT Example videos :
+https://www.youtube.com/watch?v=hDeARtZdq-U&t=639s
+
+Equation:
+u_t + u*u_x =  nu*u_xx on [-1,1] x [-1,1]
+
+Boundary Conditions: 
+Periodic (implicit via FFT)
+
+Initial Distribution :
+u(x,y,t=0) = sin(alpha*pi*x) + np.cos(beta*np.pi*-x) + 1/np.cosh(gamma*np.pi*x) 
+where alpha, beta, gamma all lie within the domain [-3,3]^3 sampled using a hypercube. 
+
+Initial Velocity Condition :
+u_t(x,y,t=0) = 0
+
+"""
+# %% 
+#Importing the required packages
+import numpy as np
+import matplotlib.pyplot as plt 
+from pyDOE import lhs 
+from Burgers_fft import *
+# %%
+n_sims = 10 #Total Number of simulation datapoints to be generated. 
+
+#Grabbing the simulation parameters from the specified domain. 
+ #alpha, beta, gamma
+lb = np.asarray([-3, -3, -3]) # Lower Bound of the parameter domain
+ub = np.asarray([3, 3, 3]) # Upper bound of the parameter domain
+
+params = lb + (ub - lb) * lhs(3, n_sims)
+
+# %% 
+from simvue import Run
+
+configuration = {'viscosity': 0.002,
+                 'domain length': 2.0,
+                 'discretisation': 1000,
+                 'dt': 0.0025,
+                 'iterations': 500,
+                 }
+# %%
+for sim in range(n_sims):
+    run = Run()
+    configuration['alpha'] = params[sim, 0]
+    configuration['beta'] = params[sim, 1]
+    configuration['gamma'] = params[sim, 2]
+    run.init(folder="/Burgers_trial", tags=['Burgers1D', 'Spectral'], metadata=configuration)
+
+    u_sol = solve_burgers(configuration) #Running the simulation with the specified configuration. 
+
+    run.save(u_sol, 'output', name='u_field') #Saving the solution as a numpy array to simvue
+
+    #Generating the spatio-temporal plot
+    fig = plt.figure()
+    plt.imshow(np.flipud(u), aspect=.8)
+    plt.axis('off')
+    plt.set_cmap('plasma')
+
+    run.save(fig, 'output', name='kymograph') #Saving the solution as a numpy array to simvue
+
+    #Simvue Artifact storage
+    run.save('Data_Gen.py', 'code', name='Data_Gen') #Saving the data generation script to simvue
+    run.save('Burgers_fft.py', 'code', name='Burgers_fft') #Saving the data generation script to simvue
+
+    run.close()
